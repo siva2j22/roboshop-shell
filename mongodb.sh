5a749bdab4085f99f42d6554330203f3d9b62cce
siva2j22 <siva.2j22@gmail.com>
2025-07-02 05:06:35 +0530

    roboshop-shell-code

--- mongodb.sh
+++ mongodb.sh
@@ -44,7 +44,7 @@ systemctl start mongod &>> LOGFILE
 
 VALIDATE $? "start mongodb"
 
-sed -i 's/127.0.0.0/0.0.0.0/g' /etc/mongod.conf &>> LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> LOGFILE
 
 VALIDATE $? "remote access"
 
