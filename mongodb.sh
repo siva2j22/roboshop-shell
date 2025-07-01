9a2a8f5659157bb6317cb6d51b0e41fc65fe7142
siva2j22 <siva.2j22@gmail.com>
2025-07-02 05:01:21 +0530

    roboshop-shell-code

---
+++ mongodb.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+G="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script start time $TIMESTAMP"  &>>LOGFILE
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo -e "$2 .. $R FAILED $N"
+    else
+        echo -e "$2 .. $G success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " $R EEROR: PLEASE RUN THIS script with root access $N"
+    exit 52 # it will stop here
+else
+    echo  -e " $G you are root user $N"
+fi     #it was used to close the if statement 
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> LOGFILE
+
+validate $? "copyied mongodb repo"
+
+yum install mongodb-org -y &>> LOGFILE
+
+validate $? "installing mongodb"
+
+systemctl enable mongod &>> LOGFILE
+
+validate $? "enabling mongodb"
+
+systemctl start mongod &>> LOGFILE
+
+validate $? "start mongodb"
+
+sed -i 's/127.0.0.0/0.0.0.0/g' /etc/mongod.conf &>> LOGFILE
+
+validate $? "remote access"
+
+systemctl restart mongod &>> LOGFILE
+
+validate $? "restart mongodb"
